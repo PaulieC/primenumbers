7d5f9fab87c67d421aba5d57f2d138c332df833c
Live session user <counci70@students.rowan.edu>
2014-10-25 13:09:43 -0400

    First commit

---
+++ primenum.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+'''
+    Checks if the provided input is a prime number
+'''
+def checkPrime(num):
+    for i in range(2, num):
+        if num%i == 0:
+            return False
+    return True
+# end checkPrime
+
+'''
+    Begin script execution
+'''
+# accept input from user
+x = input("What is the limit for prime number checks? :")
+
+# check input
+if x > 2: # number is within the range of prime numbers
+    list1 = []
+    for i in range(2,x):
+        # call prime number checker
+        if checkPrime(i):
+            # appends integer to end of list
+            list1.append(i)
+    # prints entire list
+    for item in list1:
+        print item,
+elif x == 2: # number is the lowest prime number
+    print(x)
+else: # no prime numbers found within range of input
+    print("No prime numbers found.\nCheck input!")
